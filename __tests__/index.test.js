881caa63cde9d8103bff069debc175afe9881c8d
JohnZoidy <johncom1000@mail.ru>
2022-06-16 10:53:35 +0300

    add test

---
+++ __tests__/index.test.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-restricted-globals */
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Root from '../src/main.jsx';
+
+test('Work 1', async () => {
+  render(Root());
+  expect(await screen.findByText('todos')).toBeInTheDocument();
+  await userEvent.type(screen.getByRole('textbox'), 'test task');
+  userEvent.click(screen.getByRole('button', { name: /Add task/i }));
+
+  expect(await screen.findByText('test task')).toBeInTheDocument();
+  expect(await screen.findByText('1 items left')).toBeInTheDocument();
+
+  await userEvent.type(screen.getByRole('textbox'), 'test task-2');
+  userEvent.click(screen.getByRole('button', { name: 'Add task' }));
+
+  await userEvent.type(screen.getByRole('textbox'), 'test task-3');
+  userEvent.click(screen.getByRole('button', { name: 'Add task' }));
+  expect(await screen.findByText('3 items left')).toBeInTheDocument();
+  expect(await screen.findAllByRole('checkbox')).toHaveLength(3);
+
+  await userEvent.click(screen.getAllByRole('checkbox')[1]);
+  await userEvent.click(screen.getByRole('button', { name: 'Active' }));
+  expect(await screen.getAllByRole('checkbox')).toHaveLength(2);
+
+  await userEvent.click(screen.getByRole('button', { name: 'Completed' }));
+  expect(await screen.findAllByRole('checkbox')).toHaveLength(1);
+  await userEvent.click(screen.getByRole('button', { name: 'All' }));
+  await userEvent.click(screen.getByRole('button', { name: 'Clear completed' }));
+  expect(await screen.findAllByRole('checkbox')).toHaveLength(2);
+  expect(await screen.findByText('2 items left')).toBeInTheDocument();
+});
